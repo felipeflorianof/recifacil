74f19162d3f243dc0d01958573194e05808a0eac
Felipe Floriano de Fontes <102492775+felipeflorianof@users.noreply.github.com>
2022-11-12 12:22:42 -0300

    update

---
+++ Recifacil/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+</head>
+<body>
+    <h1>Tela de home</h1>
+
+    <p><a href="login.php">Doar Material Reciclável</a></p>
+    <p><a href="loginRecycling_center.php">Centro de Reciclagem</a></p>
+    
+    
+
+</body>
+</html>
