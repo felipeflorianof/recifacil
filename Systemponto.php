0823c293674ede5d6fb96d8eccdb4963f4b0a3cf
Muryllo123 <jitave.03@gmail.com>
2022-12-12 16:08:04 -0300

    Projeto 99%

--- Systemponto.php
+++ Systemponto.php
@@ -72,7 +72,7 @@ if((!isset($_SESSION['emailponto']) == true) and (!isset($_SESSION['senhaponto']
     
 </div>
     <?php
-            echo "<dialog>";
+          
             echo "<div class='saidapop'>";
             echo "<svg xmlns='http://www.w3.org/2000/svg' width='30' height='30' fill='currentColor' class='saidapop' viewBox='0 0 16 16'>
            <path d='M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z'/>
@@ -139,8 +139,7 @@ if((!isset($_SESSION['emailponto']) == true) and (!isset($_SESSION['senhaponto']
                     
                 }
             }
-            echo "</dialog>";
-    echo "<button id='btn'>Solictações de Coleta";
+            echo "<button id='btn'>Solictações de Coleta";
             echo "</button>";
             
     ?>
